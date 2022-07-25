7aaf1b0978a8bfec7a0284121863154805f9faf5
mufHaQ <masbrowmess33@gmail.com>
2022-07-25 12:35:39 +0700

    [font2] change font size to 16

--- config.def.h
+++ config.def.h
@@ -5,12 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     "MesloLGS Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
-    "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true",
-    "FiraCode Nerd Font Mono:pixelsize=10:antialias=true:autohint=true",
+    "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true",
+    "FiraCode Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
