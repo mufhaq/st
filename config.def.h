2bbd7ac2374b7f438423af16c20d6af171054bc1
mufHaQ <masbrowmess33@gmail.com>
2022-08-29 08:54:56 +0700

    [font2] update font

--- config.def.h
+++ config.def.h
@@ -5,11 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "MesloLGS Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true",
-    //"Liberation Mono:pixelsize=16:antialias=true:autohint=true",
+    "MesloLGS Nerd Font Mono:pixelsize=16:antialias=true:autohint=true,
     //"FiraCode Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
